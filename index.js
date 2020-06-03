7f56966a8d7a16bd41d88ef4b3dd3cc415c1b4b4
prakhar19970 <prakharshukla20160@gmail.com>
2020-06-03 06:46:14 +0530

    file path changed

---
+++ index.js
@@ -0,0 +1,41 @@
+import {initializeStructure} from './memorygame.js';
+
+    $(document).ready(()=>{
+        $('#background-carousel-landscape').hide();
+        $('#background-carousel-portrait').hide();
+ 
+       console.log($(window).width());
+       
+       if($(window).width()>600)
+       {
+         $('#background-carousel-portrait').hide();
+         $('#background-carousel-landscape').fadeIn();
+       }
+       else{
+         $('#background-carousel-landscape').hide();
+       $('#background-carousel-portrait').fadeIn();
+       }
+ 
+     $(window).on('resize', function() {
+        if($(window).width() > 600) {
+         $('#background-carousel-landscape').fadeIn();
+         $('#background-carousel-portrait').fadeOut();
+         }else{
+         $('#background-carousel-landscape').fadeOut();
+         $('#background-carousel-portrait').fadeIn();
+         }
+       })
+ 
+     $('.option-btn').click((elem)=>{
+      //  elem.target.classList.toggle('button-shake');
+      $('.option-btn').attr('disabled',"true");
+      $('#myAudio').append('<source src="./assets/sounds/Blop-Mark_DiAngelo-79054334.mp3" type="audio/mp3">').attr('autoplay',true);
+
+      setTimeout(()=>{
+        initializeStructure(elem.target.getAttribute('data-size'),elem.target.getAttribute('data-mode'),$(window).width())
+        $('.option-btn').attr('disabled',"false");
+      },2000);  
+     })
+
+   })
+ 
